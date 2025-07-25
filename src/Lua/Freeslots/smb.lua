729dd72103a65acdb60bf448c0f4f0a01248158f
Sludesus <98156333+realslude@users.noreply.github.com>
2025-07-25 13:01:56 +0200

    more mario
    freeslots the new sfx and states and sprites
    adds skid sfx
    compressed some sfx

--- src/Lua/Freeslots/smb.lua
+++ src/Lua/Freeslots/smb.lua
@@ -12,14 +12,17 @@ freeslot(
 "SPR_SRWK",
 "SPR_SRJP",
 "SPR_SRDD",
+"SPR_SRSK",
 
 "MT_SO_RETRO",
 
 "S_SR_STAND",
 "S_SR_WALK",
 "S_SR_DEAD",
-"S_SR_JUMP"
+"S_SR_JUMP",
+"S_SR_SKID"
 )
 
-fsCaption("smbdea", "Mario death")
---fsCaption("smbjum", "Mario jump")
+fsCaption("smbdea", "Mario Death")
+fsCaption("smbjum", "Mario Jump")
+fsCaption("smbski", "Mario Skid")
